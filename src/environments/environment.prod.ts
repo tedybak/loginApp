5f78081b827ee89ecae20708c4c98e4ba4f7f576
Mohtadi Bakali <teddybak@hotmail.com>
2019-12-09 13:14:57 +0000

    add firebase confing into angular enviroment file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyATATTicfeXSCXDRPVsevyMeqQLhWzbD4w",
+    authDomain: "mbtloginapp.firebaseapp.com",
+    databaseURL: "https://mbtloginapp.firebaseio.com",
+    projectId: "mbtloginapp",
+    storageBucket: "mbtloginapp.appspot.com",
+    messagingSenderId: "337884779787",
+    appId: "1:337884779787:web:9e7b2dc93d9dbd49e13e0e"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyATATTicfeXSCXDRPVsevyMeqQLhWzbD4w",
+    authDomain: "mbtloginapp.firebaseapp.com",
+    databaseURL: "https://mbtloginapp.firebaseio.com",
+    projectId: "mbtloginapp",
+    storageBucket: "mbtloginapp.appspot.com",
+    messagingSenderId: "337884779787",
+    appId: "1:337884779787:web:9e7b2dc93d9dbd49e13e0e"
+  }
 };
 
 /*
