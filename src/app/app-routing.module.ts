7ad004ca81b23e49df9549878562921cea84e798
Mohtadi Bakali <teddybak@hotmail.com>
2019-12-09 00:44:25 +0000

    rutas completadas, falta manejo logout y area privada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomePageComponent } from "./componentes/home-page/home-page.component";
+import { LoginPageComponent } from "./componentes/login-page/login-page.component";
+import { RegisterPageComponent } from "./componentes/register-page/register-page.component";
+import { NotFoundPageComponent } from "./componentes/not-found-page/not-found-page.component";
+import { PrivadoPageComponent } from './componentes/privado-page/privado-page.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "/home", pathMatch: "full" },
+  { path: "login", component: LoginPageComponent },
+  { path: "home", component: HomePageComponent },
+  { path: "registrarse", component: RegisterPageComponent },
+  { path: "privado", component: PrivadoPageComponent },
+  { path: "**", component: NotFoundPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
